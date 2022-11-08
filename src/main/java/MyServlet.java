ab1900300d2acab27b0f7311c70eccde24aedfa5
PresidentialAnt <antonio.waiming@gmail.com>
2022-11-08 17:09:31 +0000

    V1

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -8,7 +8,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello");
+        resp.getWriter().write("<Header>Test</Header");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
