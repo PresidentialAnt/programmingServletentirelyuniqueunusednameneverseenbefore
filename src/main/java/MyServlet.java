2f04d9c6c6355da0edab7acdce6e4997b82d8ddf
PresidentialAnt <antonio.waiming@gmail.com>
2022-11-08 16:44:36 +0000

    V1

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MyServlet {
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+
+@WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("Hello");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
 }
